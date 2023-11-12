6855feadfd48bd365e0374ff45fe25b4562afeb3
Nasir <nasirmia12901290@gmail.com>
2023-11-12 16:44:55 +0600

    m

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+        <div className="w-28 h-28 rounded-xl" key={technology.name}>
+          {windowWidth >= 600 ? (
+            <BallCanvas icon={technology.icon} />
+          ) : (
+            <img className=" w-20 w-20 gap-2" src={technology.icon} alt={technology.name} />
+          )}
         </div>
       ))}
     </div>
